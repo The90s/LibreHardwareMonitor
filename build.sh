ebce1370b58f896846cd9755ca1a299d203f7a34
Shi Bin <15519900807@qq.com>
2023-11-01 11:50:29 +0800

    Bugfix: 构建脚本修复

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
+BUILD_TYPE=Debug
+
 case "$1" in
 r | release | Release)
     dotnet.exe build --configuration Release ./LibreHardwareMonitor.sln /target:LibreHardwareMonitorLib:rebuild /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
 
-    echo
-    echo "Build Release version"
-    echo
-    if [[ $? != 0 ]]; then
-        mv ./bin/Release/net472/* ../electron-vite/lib/
-    fi
+    BUILD_TYPE=Release
     ;;
 *)
-    dotnet.exe build ./LibreHardwareMonitor.sln /target:LibreHardwareMonitorLib:rebuild /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
+    dotnet.exe build --configuration Debug ./LibreHardwareMonitor.sln /target:LibreHardwareMonitorLib:rebuild /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
 
-    echo
-    echo "Build Debug version"
-    echo
-    if [[ $? != 0 ]]; then
-        mv ./bin/Debug/net472/* ../electron-vite/lib/
-    fi
+    BUILD_TYPE=Debug
     ;;
 esac
+
+echo
+echo "Build ${BUILD_TYPE} version"
+echo
+
+
+if [[ $? == 0 ]]; then
+    [[ ! -d "../electron-vite/lib" ]] && mkdir "../electron-vite/lib"
+    echo "Copy LibreHardwareMonitor.dll [./bin/${BUILD_TYPE}/net472/*] to electron-vite Project [../electron-vite/lib/]";
+    mv ./bin/${BUILD_TYPE}/net472/* ../electron-vite/lib/
+fi
